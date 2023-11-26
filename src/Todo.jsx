dc407da6ee928b5e7d49b93bfc4ddec1ff3b19c3
ETOMARU1210 <149168787+ETOMARU1210@users.noreply.github.com>
2023-11-26 14:54:27 +0900

    フィルターを実装

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,9 +1,12 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export const Todo = () => {
   const [todos, setTodos] = useState([]);
   const [todoTitle, setTodoTitle] = useState("");
   const [todoId, setTodoId] = useState(1);
+  const [filteredTodos, setFilteredTodos] = useState([]);
+  const [filter, setFilter] = useState("notStarted");
+
 
   const handleSetTodoTitle = (e) => {
     setTodoTitle(e.target.value);
@@ -26,20 +29,58 @@ export const Todo = () => {
     setTodos(todos.filter((todo) => todo.id !== targetTodo.id));
   };
 
+  const handleStatusChange = ({ id }, e) => {
+    const newTodos = todos.map((todo) => ({ ...todo }));
+
+    setTodos(
+      newTodos.map((todo) =>
+        todo.id === id ? { ...todo, status: e.target.value } : todo
+      )
+    );
+  };
+
+  useEffect(() => {
+    const filteringTodos = () => {
+      switch (filter) {
+        case "notStarted":
+          setFilteredTodos(
+            todos.filter((todo) => todo.status === "notStarted")
+          );
+          break;
+        case "inProgress":
+          setFilteredTodos(
+            todos.filter((todo) => todo.status === "inProgress")
+          );
+          break;
+        case "done":
+          setFilteredTodos(todos.filter((todo) => todo.status === "done"));
+          break;
+        default:
+          setFilteredTodos(todos);
+      }
+    };
+    filteringTodos();
+  }, [filter, todos]);
+
 
   return (
     <>
-      <div>
-        <input
+      <select value={filter} onChange={(e) => setFilter(e.target.value)}>
+        <option value="all">すべて</option>
+        <option value="notStarted">未着手</option>
+        <option value="inProgress">作業中</option>
+        <option value="done">完了</option>
+      </select>
+
+      <input
           type="text"
           label="タイトル"
           value={todoTitle}
           onChange={handleSetTodoTitle}
         />
-        <button onClick={() => handleAddTodo()}>作成</button>
-      </div>
+      <button onClick={() => handleAddTodo()}>作成</button>
       <ul>
-        {todos.map((todo) => (
+        {filteredTodos.map((todo) => (
           <li key={todo.id}>
             <span>{todo.title}</span>
             <select value={todo.status}>
