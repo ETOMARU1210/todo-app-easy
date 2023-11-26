8bd1cd57b2a17d1e0b7fc726a263b3936383239f
ETOMARU1210 <149168787+ETOMARU1210@users.noreply.github.com>
2023-11-26 14:31:18 +0900

    first commit

---
+++ src/Todo.jsx
@@ -0,0 +1,13 @@
+export const Todo = () => {
+  return (
+    <>
+      <ul>
+        <li>
+          <span>タイトル</span>
+          <button>編集</button>
+          <button>削除</button>
+        </li>
+      </ul>
+    </>
+  );
+};
