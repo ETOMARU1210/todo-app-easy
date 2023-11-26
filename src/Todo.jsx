169af94ee276a8ccadc89d0d345324983259a648
ETOMARU1210 <149168787+ETOMARU1210@users.noreply.github.com>
2023-11-26 14:37:33 +0900

    ステータスを定義

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,13 +1,39 @@
+import { useState } from "react";
+
 export const Todo = () => {
+  const [ todos, setTodos ] = useState(
+    [
+     {
+       title : "test",
+       status: "未着手" 
+     },
+     {
+       title : "test2",
+       status: "着手" 
+     },
+     {
+       title : "test3",
+       status: "完了" 
+     },
+   ]
+ )
   return (
     <>
       <ul>
-        <li>
-          <span>タイトル</span>
-          <button>編集</button>
-          <button>削除</button>
-        </li>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <span>{todo.title}</span>
+            <select value={todo.status}>
+              <option value="notStarted">未着手</option>
+              <option value="inProgress">作業中</option>
+              <option value="done">完了</option>
+            </select>
+            <button>編集</button>
+            <button>削除</button>
+          </li>
+        ))}
       </ul>
+      ;
     </>
   );
 };
